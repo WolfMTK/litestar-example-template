dd6c9241ee7ca851a2a4fb6fbe907a13a4be09ad
Alexander (wolf) <qrer71@yandex.ru>
2025-12-19 21:12:57 +0300

    Update `IoC`

--- src/app/ioc.py
+++ src/app/ioc.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.adapter.db.connect import get_transaction
 from app.adapter.db.gateway.task import TaskGateway
 from app.application.usecase.task import TaskUseCase
-from app.presentation.interactor import InteractorFactory
+from app.adapter.presentation.interactor import InteractorFactory
 
 
 class IoC(InteractorFactory):
