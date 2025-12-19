493bb733c9d703b25e0b3eed8000eafaf2123db4
Alexander (wolf) <qrer71@yandex.ru>
2025-12-19 21:13:17 +0300

    Update DI and add function `_get_session`

--- src/app/main.py
+++ src/app/main.py
@@ -1,23 +1,40 @@
+from typing import AsyncIterable
+
 from litestar import Litestar
+from litestar.datastructures import State
 from litestar.di import Provide
 from litestar.openapi import OpenAPIConfig
 from litestar.openapi.plugins import SwaggerRenderPlugin, RedocRenderPlugin
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
-from app.adapter.persistence.db import create_async_session_maker
 from app.config import load_config, ApplicationConfig
+from app.infrastructure.db import get_engine, get_session_maker, get_async_session
 from app.ioc import IoC
-from app.presentation.controllers.task import TaskController
+from app.adapter.presentation.controllers import TaskController
+
+
+async def _get_session(state: State) -> AsyncIterable[AsyncSession]:
+    session_maker: async_sessionmaker[AsyncSession] = state.session_maker
+    async for session in get_async_session(session_maker):
+        yield session
 
 
 def create_app() -> Litestar:
     config = load_config()
 
+    engine = get_engine(config.db.url)
+    session_maker = get_session_maker(engine)
+
     app = Litestar(
         debug=config.debug,
         route_handlers=[TaskController],
-        dependencies=_init_dependencies(config),
+        dependencies={
+            "session": Provide(_get_session),
+            "ioc": Provide(IoC, sync_to_thread=True),
+        },
         openapi_config=_init_openapi_config()
     )
+    app.state.session_maker = session_maker
     return app
 
 
