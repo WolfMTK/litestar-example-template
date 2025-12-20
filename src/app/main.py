280e17e291d0c29405c4fa41a6a890811e419ff1
Alexander (wolf) <qrer71@yandex.ru>
2025-12-20 12:54:50 +0300

    Rename function `create_app` to `get_app`

--- src/app/main.py
+++ src/app/main.py
@@ -1,69 +1,42 @@
-from typing import AsyncIterable
-
+from dishka import make_async_container
+from dishka.integrations.litestar import setup_dishka
 from litestar import Litestar
-from litestar.datastructures import State
-from litestar.di import Provide
 from litestar.openapi import OpenAPIConfig
 from litestar.openapi.plugins import SwaggerRenderPlugin, RedocRenderPlugin
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
-from app.config import load_config, ApplicationConfig
-from app.infrastructure.db import get_engine, get_session_maker, get_async_session
-from app.ioc import IoC
 from app.adapter.presentation.controllers import TaskController
+from app.config import load_config, ApplicationConfig
+from app.provider import AppProvider
 
 
-async def _get_session(state: State) -> AsyncIterable[AsyncSession]:
-    session_maker: async_sessionmaker[AsyncSession] = state.session_maker
-    async for session in get_async_session(session_maker):
-        yield session
-
-
-def create_app() -> Litestar:
+def get_app() -> Litestar:
     config = load_config()
-
-    engine = get_engine(config.db.url)
-    session_maker = get_session_maker(engine)
-
+    container = make_async_container(
+        AppProvider(),
+        context={
+            ApplicationConfig: config,
+        },
+    )
     app = Litestar(
         debug=config.debug,
         route_handlers=[TaskController],
-        dependencies={
-            "session": Provide(_get_session),
-            "ioc": Provide(IoC, sync_to_thread=True),
-        },
-        openapi_config=_init_openapi_config()
+        openapi_config=OpenAPIConfig(
+            title="Example Service",
+            description="Example service",
+            version="0.0.1",
+            render_plugins=[
+                SwaggerRenderPlugin(),
+                RedocRenderPlugin(),
+            ],
+            # components=Components(
+            #     security_schemes={
+            #         "BearerToken": SecurityScheme(
+            #             type="http",
+            #             scheme="bearer"
+            #         )
+            #     }
+            # )
+        ),
     )
-    app.state.session_maker = session_maker
+    setup_dishka(container=container, app=app)
     return app
-
-
-def _init_openapi_config() -> OpenAPIConfig:
-    config = OpenAPIConfig(
-        title="Example Service",
-        description="Example service",
-        version="0.0.1",
-        render_plugins=[
-            SwaggerRenderPlugin(),
-            RedocRenderPlugin(),
-        ],
-        # components=Components(
-        #     security_schemes={
-        #         "BearerToken": SecurityScheme(
-        #             type="http",
-        #             scheme="bearer"
-        #         )
-        #     }
-        # )
-    )
-    return config
-
-
-def _init_dependencies(config: ApplicationConfig) -> dict[str, Provide]:
-    db_config = config.db
-
-    dependencies = {
-        "session": Provide(create_async_session_maker(db_config.url)),
-        "ioc": Provide(IoC, sync_to_thread=True),
-    }
-    return dependencies
