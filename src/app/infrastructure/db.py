dd77d6f0fe1ff69040e25206a7ce47ea5003a276
Alexander (wolf) <qrer71@yandex.ru>
2025-12-20 12:53:42 +0300

    Delete function `get_async_session`

--- src/app/infrastructure/db.py
+++ src/app/infrastructure/db.py
@@ -1,5 +1,3 @@
-from typing import AsyncIterable
-
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession
 
 
@@ -11,8 +9,3 @@ def get_engine(url: str) -> AsyncEngine:
 def get_session_maker(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
     session_maker = async_sessionmaker(engine, expire_on_commit=False)
     return session_maker
-
-
-async def get_async_session(session_maker: async_sessionmaker) -> AsyncIterable[AsyncSession]:
-    async with session_maker() as session:
-        yield session
