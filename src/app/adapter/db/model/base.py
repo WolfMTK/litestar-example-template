375f31743cb3788898ee15a46eb5258ab4f86332
Alexander (wolf) <qrer71@yandex.ru>
2025-12-20 12:53:28 +0300

    Update `BaseModel`

--- src/app/adapter/db/model/base.py
+++ src/app/adapter/db/model/base.py
@@ -1,7 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy.orm import DeclarativeBase
 
 
 class BaseModel(DeclarativeBase):
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+    pass
