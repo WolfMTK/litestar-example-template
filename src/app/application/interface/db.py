23c818946274654640218847f9e7b796b96bc4a2
Alexander (wolf) <qrer71@yandex.ru>
2025-12-20 12:52:50 +0300

    Rename class `Transaction` to `DBSession`

--- src/app/application/interface/db.py
+++ src/app/application/interface/db.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 from typing import Protocol
 
 
-class Transaction(Protocol):
+class DBSession(Protocol):
     @abstractmethod
     async def commit(self) -> None: ...
 
