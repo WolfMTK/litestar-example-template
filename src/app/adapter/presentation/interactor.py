0a77ae91edcc980546960b64cd98093dfb6098bc
Alexander (wolf) <qrer71@yandex.ru>
2025-12-20 12:52:35 +0300

    Delete files

--- src/app/adapter/presentation/interactor.py
+++
@@ -1,9 +0,0 @@
-from abc import abstractmethod, ABC
-from contextlib import AbstractAsyncContextManager
-
-from app.application.usecase.task import TaskUseCase
-
-
-class InteractorFactory(ABC):
-    @abstractmethod
-    def task_usecase(self) -> AbstractAsyncContextManager[TaskUseCase]: ...
